ffc5ffb96f2a3db1c41c32cbc1d9521b667fb04f
JunnanJii <775691562@qq.com>
2023-06-08 01:27:32 +0800

    0127

---
+++ main.js
@@ -0,0 +1,17 @@
+const btn = document.querySelector('#egg4');
+
+function egg44() {
+  document.body.a.#egg4.src="资源21-8.png"
+	document.body.style.color = "#FFFFFF";
+}
+
+btn.mouseenter = darkMode;
+
+const btn = document.querySelector('#egg4');
+
+function normal() {
+  document.body.style.backgroundColor = "#161616";
+	document.body.style.color = "#FFFFFF";
+}
+
+btn.mouseout = darkMode;
